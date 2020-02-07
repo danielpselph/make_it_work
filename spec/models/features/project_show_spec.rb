85682c0715519c16dacf1eaf199047b77d9b8160
Daniel Selph <daniel.p.selph@gmail.com>
2020-02-07 11:08:20 -0700

    Add project show page, project controller and add routes to project index

--- spec/models/features/project_show_spec.rb
+++ spec/models/features/project_show_spec.rb
@@ -11,9 +11,7 @@ require 'rails_helper'
 RSpec.describe 'As a visitor' do
     describe 'When I visit the projects show page' do
         before :each do
-            @project1 = Project.create(name: "Litfit",
-                                        material: "Lamp Shade",
-                                        challenge_theme: "Apartment Furnishings")
+            @project1 = Project.create(name: "Litfit", material: "Lamp Shade",)
         end 
 
         it "I see that projects name, material and theme of the challenge it belongs to" do
@@ -24,7 +22,7 @@ RSpec.describe 'As a visitor' do
             
             expect(page).to have_content(@project1.name)
             expect(page).to have_content(@project1.material)
-            expect(page).to have_content(@project1.challenge_theme)
+            expect(page).to have_content(@project1.challenge.theme)
         end
     end
 end
